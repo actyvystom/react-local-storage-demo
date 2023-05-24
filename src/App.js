efb234344f412405eafbbbdb997757954ed562be
actyvystom <thomas.goerldt@actyvyst.com>
2023-05-24 17:45:20 +0200

    local storage demo end, commented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
 import List from "./components/List";
 import Form from "./components/Form";
 import "./styles.css";
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
-
+  // declare a state variable using the useLocalStorageState hook (installed as 3rd party dependency via 'npm install use-local-storage-state' )
+  const [todos, setTodos] = useLocalStorageState("todos", {
+    defaultValue: [],
+  });
+  // declare a handler function to pass as prop to our form component
   function handleAddTodo(title) {
+    // use the state setter as usual (treat state as immutable, so we create a copy of the existing values) to add our new todo
     setTodos([
       {
         id: uid(),
@@ -17,8 +22,9 @@ export default function App() {
       ...todos,
     ]);
   }
-
+  // declare a handler function we pass as prop to our List component
   function handleToggleCheckTodo(idToToggle) {
+    // use the setter function to update our todo object in state (and local storage, thanks to useLocalStorage hook)
     setTodos(
       todos.map((todo) =>
         todo.id === idToToggle ? { ...todo, isChecked: !todo.isChecked } : todo
